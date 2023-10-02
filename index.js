0ca12cc1051e685abdf6273c14f576709266e7f6
lanmower <almagestfraternite@gmail.com>
2023-10-02 11:46:26 +0000

    module fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-const ipc = require('hyper-ipc-secure');
-require('dotenv').config();
+const ipc = require('hyper-ipc-secure')
+require('dotenv').config()
 const runCall = (input)=>{
-  console.log(input);
-  return input;
+  console.log(input)
+  return input
 }
 const init = (kp, node = ipc(), serverKey = node.getSub(kp, process.env.SERVERNAME), callKey = node.getSub(kp, process.env.IPCNAME)) => {
-  node.lbserve(callKey, serverKey, process.env.IPCNAME, runCall);
-  return node;
+  node.lbserve(callKey, serverKey, process.env.IPCNAME, runCall)
+  return node
 }
-export default init;
+module.exports = init

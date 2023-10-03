e71ddeaccc74d4b4f15038378bf90ad1fb2f27ac
lanmower <almagestfraternite@gmail.com>
2023-10-03 19:10:02 +0000

    jsdoc inclusion

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * Executes a function, input object returns.
+ *
+ * @param {object} input - The input object to be logged and returned.
+ * @returns {object} The same input object that was provided as a parameter.
+ */
 const runCall = (input)=>{
   console.log(input)
   return input

fdfd0ddfa44f518bd24eefe32bde2726fe005a8f
lanmower <almagestfraternite@gmail.com>
2023-10-03 12:29:00 +0000

    done

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const axios = require("axios");
-const runCall = async input => {
+
+const runCall = async (input) => {
   try {
-    const response = await axios.get(`https://www.google.com/search?q=${input.search}`);
-    console.log(response.data);
-    return {response:response.data}
+    const response = await axios.get(`https://www.googleapis.com/customsearch/v1?key=${input.apiKey}&cx=${input.cx}&q=${input.search}`);
+    return {
+      data: response.data
+    };
   } catch (error) {
     console.error(error);
-    return null
+    return null;
   }
 };
+
 module.exports = runCall;

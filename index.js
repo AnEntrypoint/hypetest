f3e138921f4de6f87c0da5f1a76564881d542839
lanmower <almagestfraternite@gmail.com>
2023-10-03 19:09:47 +0000

    jsdoc inclusion

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const axios = require("axios");
-
+/**
+ * Performs a Google Custom Search API call.
+ * @param {Object} input - The input object.
+ * @param {string} input.search - The search query.
+ * @returns {Promise<Object>} - A promise that resolves to the response data object.
+ */
 const runCall = async (input) => {
   try {
-    const response = await axios.get(`https://www.googleapis.com/customsearch/v1?key=${input.apiKey}&cx=${input.cx}&q=${input.search}`);
+    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_API_KEY}&q=${input.search}`);
+    const data = await response.json();
     return {
-      data: response.data
+      data
     };
   } catch (error) {
     console.error(error);
